9b521b4e929fb64c81eb0ded58ba006b467d8ddb
Zakariyya Mughal <zaki.mughal@gmail.com>
2013-01-13 13:24:15 -0600

    first attempt at http://techtalks.tv/ support
    problem: frequent RTMP pause/play which breaks frames

---
+++ lib/FlashVideo/Site/Techtalks.pm
@@ -0,0 +1,31 @@
+# Part of get-flash-videos. See get_flash_videos for copyright.
+package FlashVideo::Site::Techtalks;
+
+use strict;
+use FlashVideo::Utils;
+
+sub find_video {
+  my ($self, $browser) = @_;
+
+  my $author = ($browser->content =~ /<li class="authors">([^<]+)/s)[0];
+  my $title  = ($browser->content =~ /<h2 class="title">([^<]+)/)[0];
+
+  my $streamer = ($browser->content =~ /netConnectionUrl:\s*["']([^"']+)/)[0];
+  my $playpath = ($browser->content =~ /<a class="presenter" href=["']([^"']+)/)[0];
+  my $swfUrl = 'http://techtalks.tv' .
+	  ($browser->content =~ m{"a\.presenter",\s+["']([^"']+)})[0];
+  # NOTE This will not download the slides
+
+  my $data = {
+    app      => (split m{/}, $streamer)[-1],
+    rtmp     => $streamer,
+    swfUrl   => $swfUrl,
+    playpath => $playpath,
+    pageUrl  => 'http://techtalks.tv',
+    flv      => title_to_filename("$author - $title"),
+  };
+
+  return $data;
+}
+
+1;
